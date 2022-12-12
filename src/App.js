f5abe6c9bb58665dc53e6e98279afe9fefd70d71
yoontsy2000 <yoontsy2000@gmail.com>
2022-12-12 14:10:33 +0000

    Added working SearchBar

--- src/App.js
+++ src/App.js
@@ -2,32 +2,28 @@ import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 
 const API_KEY = process.env.REACT_APP_API_KEY
+let movie_id = 550
 
 function App() {
   const [data, setData] = useState(null);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  // This function is called when the search term is updated
-  const handleSearchTermChange = newSearchTerm => {
-    setSearchTerm(newSearchTerm);
-  };
+  const [searchResult, setSearchResult] = useState([]);
 
   // use the useEffect hook to fetch the data when the component is mounted
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`);
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}`);
       const data = await response.json();
       setData(data);
     }
 
     fetchData();
+    console.log(searchResult);
   }, []);
 
   return (
     <div className="App">
       <SearchBar
-        searchTerm={searchTerm}
-        onSearchTermChange={handleSearchTermChange}
+        setSearchResult={setSearchResult}
       />
       {data ? (
         <div>
@@ -35,11 +31,17 @@ function App() {
           <h1>{data.title}</h1>
           <p>{data.overview}</p>
           <p>Release Date: {data.release_date}</p>
-          <img src={data.poster_path} alt={data.title} />
+          <img src={"http://image.tmdb.org/t/p/w200/" + data.poster_path} alt={data.title} />
         </div>
       ) : (
         <div>Loading...</div>
       )}
+      {searchResult.map(result => (
+        <div key={result.id}>
+          <h1>{result.title}</h1>
+          {/* display the rest of the result data here */}
+        </div>
+      ))}
     </div>
   );
 }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function SearchBar(props) {
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+function SearchBar({ setSearchResult }) {
   // Declare a new state variable for the search term
-  const [searchTerm, setSearchTerm] = useState(props.searchTerm);
+  const [searchTerm, setSearchTerm] = useState("");
 
   // This function is called when the user clicks the search button
-  const handleSearch = () => {
+  const handleSearch = async () => {
     // Make an API call to search for the specified term here
-    console.log(this.searchTerm);
+    const query = encodeURIComponent(searchTerm)
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`);
+    const data = await response.json();
+    setSearchResult(data.results);
   };
 
   return (
@@ -17,7 +22,6 @@ function SearchBar(props) {
         value={searchTerm}
         onChange={e => {
           setSearchTerm(e.target.value);
-          props.onSearchTermChange(e.target.value);
         }}
       />
       <button onClick={handleSearch}>Search</button>
